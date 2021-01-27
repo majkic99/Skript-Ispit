88e0b8fb03d027aacd32578347951d8f47c1f135
majkic99 <60391069+majkic99@users.noreply.github.com>
2021-01-28 00:28:20 +0100

    gui ostao

--- ispitDjango/ispitDjango/views.py
+++ ispitDjango/ispitDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Fudbaleri, Timovi, Users
 
 def stats(req):
-    timovi = Timovi.objects.all()
-    fudbaleri = Fudbaleri.objects.all()
-    korisnici = Users.objects.all()
+    timovi = Timovi.objects.all().order_by('-osvojeni_poeni')
+    fudbaleri = Fudbaleri.objects.all().order_by('-golovi').order_by('-asistencije')
+    korisnici = Users.objects.all().order_by('name')
     return render(req, 'stats.html',{"timovi" : timovi, "fudbaleri" : fudbaleri, "korisnici" : korisnici})
